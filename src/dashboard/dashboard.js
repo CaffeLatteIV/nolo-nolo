f4278473a7cdbbfe712c8af618550cabd2fc27a3
Mattia <mattiababbini99@gmail.com>
2021-11-14 11:03:35 +0100

    me ne sbatto degli errori

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -5,23 +5,44 @@ import './css/dashboard.css'
 class Dashboard extends React.Component {
   render () {
     return (
-      <div className="row">
-      <section className="row col-4">
-        <this.Panel title="Fatturato" price={342156} change={+4.5}/>
-        <this.Panel title="Fatturato" price={342156} change={+4.5}/>
-      </section>
-      <article className="col-8 panel">cc</article>
+      <div className="container-fluid overflow-hidden">
+        <section className="row gx-5x">
+          <this.SummaryPanel title="Fatturato" price={342156} change={4.5} />
+          <this.SummaryPanel title="Fatturato" price={342156} change={-4.5} />
+          <this.SummaryPanel title="Fatturato" price={342156} change={4.5} />
+          <this.SummaryPanel title="Fatturato" price={342156} change={4.5} />
+        </section>
+        <section className="row g-2">
+          <this.PiePanel title="Grafico torta"/>
+          <this.PiePanel title="Grafico torta"/>
+
+        </section>
       </div>
     )
   }
 
-  Panel ({ title, price, change }) {
+  SummaryPanel ({ title, price, change }) {
+    let update = (
+      <p><span className="positiveUpdate"><span className="material-icons upArrow">forward</span>
+      {change} </span>dall&apos;ultimo update</p>)
+    if (change < 0) {
+      update = (
+      <p><span className="negativeUpdate"><span className="material-icons downArrow">forward</span>
+      {change} </span>dall&apos;ultimo update</p>)
+    }
     return (
-    <article className="col panel mx-2">
-      <p>{title}</p>
-      <h2>€{price}</h2>
-      <p>{change}</p>
-    </article>
+      <article className="col panel">
+        <span className="p-3">
+        <p>{title}</p>
+        <h2 className="price">€{price}</h2>
+        {update}</span>
+      </article>
+    )
+  }
+
+  PiePanel ({ title }) {
+    return (
+      <article className="col-3 panel"><h5>{title}</h5></article>
     )
   }
 }
