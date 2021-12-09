ae1a3bdfe60c8df84d64524b873c308d2c5fe026
Mattia <mattiababbini99@gmail.com>
2021-12-09 09:11:38 +0100

    aggiunto /find/user find/product /find/date /find

--- backend/src/v1/rental.js
+++ backend/src/v1/rental.js
@@ -1,40 +1,77 @@
 import Express from 'express'
-import bcrypt from 'bcrypt'
-import Database from '../database/database.js'
-import loggerWrapper from '../logger.js'
+import Database from '../database/rental'
+import loggerWrapper from '../logger'
 
-const logger = loggerWrapper('API')
+const logger = loggerWrapper('Rental API')
 const db = new Database()
 const app = Express.Router()
 
-app.post('/rental', async (req, res) => {
+app.post('/add', async (req, res) => {
   try {
     const { item } = req.body
     await db.addRentals(item)
-    return res.status(200).send({ code: 200, msg: 'Rent added' })
+    return res.status(200).send({ code: 200, msg: 'Rental added' })
   } catch (err) {
     logger.error(err.message)
     logger.error(err.stack)
     return res.status(500).send({ code: 500, msg: 'There was an error while uploading data, try again' })
   }
 })
-app.get('/rental', async (req, res) => {
+app.get('/find/user', async (req, res) => {
   try {
     const { item } = req.body
-    const rent = await db.findRentals(item)
-    if (rent) return res.status(200).send(rent)
-    return res.status(404).send({ code: 400, msg: 'Not found' })
+    const rent = await db.findUserRentals(item.clientCode)
+    // TODO: verificare che restituisca null e non [null]
+    if (rent === null) return res.status(404).send({ code: 400, msg: 'Not found' })
+    return res.status(200).send(rent)
   } catch (err) {
     logger.error(err.message)
     logger.error(err.stack)
     return res.status(500).send({ code: 500, msg: 'There was an error while performing the request, try again' })
   }
 })
-async function generateHash(message) {
-  // create hash algorithm
-  const salt = await bcrypt.genSalt(10)
-  const hashHex = await bcrypt.hash(message, salt)
+app.get('/find/product', async (req, res) => {
+  try {
+    const { item } = req.body
+    const rent = await db.findProductRentals(item.productCode)
+    // TODO: verificare che restituisca null e non [null]
+    if (rent === null) return res.status(404).send({ code: 400, msg: 'Not found' })
+    // TODO: verificare che restituisca un oggetto json e non un array e basta
+    return res.status(200).send(rent)
+  } catch (err) {
+    logger.error(err.message)
+    logger.error(err.stack)
+    return res.status(500).send({ code: 500, msg: 'There was an error while performing the request, try again' })
+  }
+})
 
-  return hashHex
-}
+app.get('/find/date', async (req, res) => {
+  try {
+    const { item } = req.body
+    const rent = await db.findRecentEndings(item.date)
+    // TODO: verificare che restituisca null e non [null]
+    if (rent === null) return res.status(404).send({ code: 400, msg: 'Not found' })
+    // TODO: verificare che restituisca un oggetto json e non un array e basta
+    return res.status(200).send(rent)
+  } catch (err) {
+    logger.error(err.message)
+    logger.error(err.stack)
+    return res.status(500).send({ code: 500, msg: 'There was an error while performing the request, try again' })
+  }
+})
+app.get('/find', async (req, res) => {
+  try {
+    const { item } = req.body
+    const rent = await db.find(item.productCode, item.clientCode)
+    // TODO: verificare che restituisca null e non [null]
+    if (rent === null) return res.status(404).send({ code: 400, msg: 'Not found' })
+    // TODO: verificare che restituisca un oggetto json e non un array e basta
+    return res.status(200).send(rent)
+  } catch (err) {
+    logger.error(err.message)
+    logger.error(err.stack)
+    return res.status(500).send({ code: 500, msg: 'There was an error while performing the request, try again' })
+  }
+})
+
 export default app
