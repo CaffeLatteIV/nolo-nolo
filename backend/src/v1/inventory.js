269246ae13f27322af8be0c7ba588783d64cc527
Mattia <mattiababbini99@gmail.com>
2021-12-09 09:08:32 +0100

    aggiunto /findOne /category /product

--- backend/src/v1/inventory.js
+++ backend/src/v1/inventory.js
@@ -1,20 +1,26 @@
 import Express from 'express'
-import Database from '../database/database.js'
-import loggerWrapper from '../logger.js'
+import Database from '../database/inventory'
+import loggerWrapper from '../logger'
 
 const db = new Database()
 const logger = loggerWrapper('Inventory API')
 const app = Express.Router()
 
-app.get('/inventory', async (req, res) => {
+app.get('/findOne', async (req, res) => {
   const { item } = req.body
   logger.info(`Finding item ${item.title}`)
-  await db.connect()
-  const prod = await db.findInventory(item.title)
+  const prod = await db.findOneAvailable(item.title, item.category, item.id)
   if (prod === null) return res.status(404).send({ code: 404, msg: 'Item not available' })
   return res.status(200).send(prod)
 })
-app.post('/inventory', async (req, res) => {
+app.get('/category', async (req, res) => {
+  const { item } = req.body
+  logger.info(`Finding items of ${item.category} category`)
+  const prod = await db.findAll(item.category, item.available)
+  if (prod === null) return res.status(404).send({ code: 404, msg: 'Item not available' })
+  return res.status(200).send(prod)
+})
+app.post('/product', async (req, res) => {
   try {
     const { item } = req.body
     logger.info(`Adding: ${item.title}`)
@@ -23,6 +29,7 @@ app.post('/inventory', async (req, res) => {
     return res.status(200).send({ code: 200, msg: 'ok' })
   } catch (err) {
     logger.error(err.message)
+    logger.error(err.stack)
     return res.status(500).send({ code: 500, msg: 'Internal server error' })
   }
 })
