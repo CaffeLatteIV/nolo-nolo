1b0e9a48adf3c57b65f27b1aa077daf24181f8f3
Mattia <mattiababbini99@gmail.com>
2021-12-09 09:07:25 +0100

    aggiunto /logn /register /lookup

--- backend/src/v1/client.js
+++ backend/src/v1/client.js
@@ -1,17 +1,17 @@
 import Express from 'express'
-import Database from '../database/database.js'
-import loggerWrapper from '../logger.js'
+// import bcrypt from 'bcrypt'
+import Client from '../database/client'
+import loggerWrapper from '../logger'
 
-const db = new Database()
+const db = new Client()
 const logger = loggerWrapper('Client API')
 const app = Express.Router()
 
-app.post('/client', async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
     const { item } = req.body
     logger.info(`Adding: ${item.username}`)
-    await db.connect()
-    db.addClients(item)
+    await db.addClients(item)
     return res.status(200).send({ code: 200, msg: 'ok' })
   } catch (err) {
     logger.error(err.message)
@@ -19,12 +19,28 @@ app.post('/client', async (req, res) => {
   }
 })
 
-app.get('/client', async (req, res) => {
+app.get('/login', async (req, res) => {
   const { item } = req.body
   logger.info(`Finding user ${item.username}`)
   await db.connect()
-  const user = await db.findClient(item.username)
-  if (user === null) return res.status(404).send({ code: 404, msg: 'Client not found' })
+  const user = await db.findClient(item.username, item.password)
+  if (user === null) return res.status(404).send({ code: 404, msg: 'User not registered' })
   return res.status(200).send(user)
 })
+app.get('/lookup', async (req, res) => {
+  // TODO: accesso riservato solo a amministratori
+  const { item } = req.body
+  logger.info(`Finding user ${item.username}`)
+  await db.connect()
+  const user = await db.lookupClient(item.username)
+  if (user === null) return res.status(404).send({ code: 404, msg: 'User not registered' })
+  return res.status(200).send(user)
+})
+// async function generateHash(message) {
+//   // create hash algorithm
+//   const salt = await bcrypt.genSalt(10)
+//   const hashHex = await bcrypt.hash(message, salt)
+
+//   return hashHex
+// }
 export default app
