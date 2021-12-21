bfb40f3532d9616f1e3143b60fd2e37ad6061876
bosslibra <alessandr.libralesso@studio.unibo.it>
2021-12-21 12:21:54 +0100

    commit for testing purposes

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -82,7 +82,7 @@ function Content () {
             </button>
         </div>
         <div className="container-fluid row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 p-0 m-0" id="card-container">
-            { Array.from({ length: 10 }, (_, i) => (
+            { Array.from({ length: 30 }, (_, i) => (
                     <div className="col p-1">
                         <ProductCard/>
                     </div>
