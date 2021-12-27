e63c693f41fe259336c562fb9b631d5167701e97
bosslibra <alessandr.libralesso@studio.unibo.it>
2021-12-27 19:05:30 +0100

    forgot Link in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,9 +8,9 @@ function Navbar () {
         {/* Main navbar */}
         <nav className="navbar navbar-expand md-04dp">
             <div className="container-fluid">
-                <a className="navbar-brand text-white" title="Menù principale" href="#">
+                <Link to='/' className="navbar-brand text-white" title="Menù principale">
                     NOLONOLO
-                </a>
+                </Link>
                 <button className="navbar-toggler"type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
