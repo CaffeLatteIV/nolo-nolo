1770f0fc4d78e521f4dae413e137bbc23c1e3d7a
Mattia <mattiababbini99@gmail.com>
2022-01-07 10:53:29 +0100

    rimosso middleware inutile + aggiunto middleware per immagini

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import inventory from './v1/inventory-api.js'
 import client from './v1/client-api.js'
 import employee from './v1/employee-api.js'
 import token from './v1/token-api.js'
+import image from './v1/image-api.js'
 
 const logger = loggerWrapper('API')
 const app = Express()
@@ -17,23 +18,12 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   return next()
 })
-// error handling
-app.use((req, res, next) => {
-  if (req.body === undefined) {
-    logger.error('Request body undefined')
-    return res.status(404).send({ code: 404, msg: 'request is undefined' })
-  }
-  // if (req.body.item === undefined) {
-  //   logger.error('Request item undefined')
-  //   return res.status(404).send({ code: 404, msg: 'request body is undefined' })
-  // }
-  return next()
-})
 
+app.use('/v1', token)
 app.use('/v1/rentals', rental)
 app.use('/v1/inventories', inventory)
 app.use('/v1/clients', client)
 app.use('/v1/employee', employee)
-app.use('/v1', token)
+app.use('/v1/image', image)
 
 app.listen(PORT, () => logger.info(`Listening on port ${PORT}`))
