eae7fe6228c023de81097be5abf38adf7c7045b0
Mattia <mattiababbini99@gmail.com>
2022-01-12 16:59:07 +0100

    creata pagine dove mostrare prodotti della categoria cercata

---
+++ frontend/frontoffice/src/pages/CategoryPage.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useLocation, useNavigate } from 'react-router-dom'
+import ProductCard from '../components/ProductCard.js'
+
+const URL = process.env.INVENTORY_URL || 'http://localhost:5000/v1/inventories'
+function CategoryPage() {
+  const [products, setProducts] = useState(undefined)
+  const { search } = useLocation()
+  // viene eseguito una volta sola (alla prima visita della pagina)
+  const query = new URLSearchParams(search)
+  const category = query.get('id')
+  // se il parametro id non è presente restituisce la pagina 404
+  if (category === null) {
+    const navigate = useNavigate()
+    navigate('*')
+  }
+  useEffect(async () => {
+    const { data } = await axios({
+      method: 'GET',
+      url: `${URL}/categories/${category}`,
+    })
+    setProducts(data.products)
+  }, [])
+
+  document.title = 'NOLONOLO'
+  return (
+    <div className="container p-2">
+      <div className="container-fluid row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 p-0 m-0" id="card-container">
+        { products ? products.map(({ _id, title, price, condition, media }) => (
+          <div key={_id} className="col p-1">
+            <ProductCard id={_id} title={title} condition={condition} price={price.weekday} img={media.img} />
+          </div>
+        )) : 'caricamento...'}
+      </div>
+    </div>
+
+  )
+}
+
+export default CategoryPage
