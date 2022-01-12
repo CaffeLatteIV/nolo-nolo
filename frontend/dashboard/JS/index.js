92ea25e47348dcc552a4dc2afc3ff2c4f6dbb172
bosslibra <alessandrolibralesso@gmail.com>
2022-01-12 18:26:19 +0100

    rename graphs and add other graph

--- frontend/dashboard/JS/index.js
+++ frontend/dashboard/JS/index.js
@@ -15,10 +15,10 @@ const lineConfig = {
   data: lineData,
   options: {},
 };
-var myChart = new Chart(document.getElementById("lineChart"), lineConfig);
+var lineChart = new Chart(document.getElementById("lineChart"), lineConfig);
 
 //Setup for Doughnut Chart --> Contiene dati su disponibilità
-const doughnutData = {
+const distrData = {
   labels: ["Noleggiati", "Disponibili", "Prenotati"],
   datasets: [
     {
@@ -33,23 +33,53 @@ const doughnutData = {
     },
   ],
 };
-const doughnutConfig = {
+const distrConfig = {
   type: "doughnut",
-  data: doughnutData,
+  data: distrData,
   options: {
     responsive: false,
   },
 };
-var myChart = new Chart(document.getElementById("pieChart"), doughnutConfig);
+var distrChart = new Chart(document.getElementById("distrChart"), distrConfig);
 
-//TODO: 
+//Setup for Doughnut Chart --> Contiene dati su condizioni
+const condizioniData = {
+  labels: ["Ottimi", "Buoni", "Parzialmente Danneggiati"],
+  datasets: [
+    {
+      label: "Condizioni oggetti",
+      data: [330, 500, 120],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 0,
+    },
+  ],
+};
+const condizioniConfig = {
+  type: "doughnut",
+  data: condizioniData,
+  options: {
+    responsive: false,
+  },
+};
+var condizioniChart = new Chart(document.getElementById("condizioniChart"), condizioniConfig);
 
-//Inject values to make doughnut chart canvas accessible
-$(document).ready(function(){
-    var numOggettiNoleggiati = doughnutData.datasets[0].data[0]
-    var numOggettiDisponibili = doughnutData.datasets[0].data[1]
-    var numOggettiPrenotati = doughnutData.datasets[0].data[2]
-    $('#valoreOggettiInNoleggio').html(numOggettiNoleggiati)
-    $('#valoreOggettiDisponibili').html(numOggettiDisponibili)
-    $('#valoreOggettiPrenotati').html(numOggettiPrenotati)
+$(document).ready(function () {
+  //Inject values to make "Distribuzione" chart canvas accessible
+  var numOggettiNoleggiati = distrData.datasets[0].data[0]
+  var numOggettiDisponibili = distrData.datasets[0].data[1]
+  var numOggettiPrenotati = distrData.datasets[0].data[2]
+  $('#valoreOggettiInNoleggio').html(numOggettiNoleggiati)
+  $('#valoreOggettiDisponibili').html(numOggettiDisponibili)
+  $('#valoreOggettiPrenotati').html(numOggettiPrenotati)
+  //Inject values to make "Condizioni" chart canvas accessible
+  var numOttimi = condizioniData.datasets[0].data[0]
+  var numBuono = condizioniData.datasets[0].data[1]
+  var numParzDanneggiati = condizioniData.datasets[0].data[2]
+  $('#valoreOttimeCond').html(numOttimi)
+  $('#valoreBuoneCond').html(numBuono)
+  $('#valoreParzDanneggiati').html(numParzDanneggiati)
 })
