37cf9b753e71ff48924be8a1591e8982b766aaf0
Mattia <mattiababbini99@gmail.com>
2022-01-14 21:43:22 +0100

    aggiunta route per pagina receipt

--- frontend/frontoffice/src/App.js
+++ frontend/frontoffice/src/App.js
@@ -10,7 +10,7 @@ import GoTop from './components/GoTop.js'
 import Homepage from './pages/Homepage.js'
 import Login from './pages/Login.js'
 import ProductPage from './pages/ProductPage.js'
-import Cart from './pages/Cart.js'
+import Receipt from './pages/Receipt.js'
 import NotFound from './pages/NotFound.js'
 import Orders from './pages/Orders.js'
 import CategoryPage from './pages/CategoryPage.js'
@@ -47,8 +47,9 @@ function App() {
           <Route path="/login" element={<Login isLogging />} />
           <Route path="/register" element={<Login isLogging={false} />} />
           <Route path="/product" element={<RequireAuth><ProductPage /></RequireAuth>} />
-          <Route path="/cart" element={<RequireAuth><Cart /></RequireAuth>} />
+          {/* <Route path="/cart" element={<RequireAuth><Cart /></RequireAuth>} /> */}
           <Route path="/orders" element={<RequireAuth><Orders /></RequireAuth>} />
+          <Route path="/receipt" element={<RequireAuth><Receipt /></RequireAuth>} />
           <Route path="/category" element={<CategoryPage />} />
           <Route path="/account" element={<RequireAuth><Account /></RequireAuth>} />
           <Route path="*" element={<NotFound />} />
