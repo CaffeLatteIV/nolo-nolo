42498647a2a11960200084ff722be071033fbda0
Mattia <mattiababbini99@gmail.com>
2022-01-14 21:42:37 +0100

    da rimuovere

--- frontend/frontoffice/src/pages/Cart.js
+++ frontend/frontoffice/src/pages/Cart.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Cookies from 'universal-cookie'
 import CartItem from '../components/CartItem.js'
 
 function Cart() {
   document.title = 'NOLONOLO Carello'
-  const arrayOfProducts = useState(
-    {
-      id: 0,
-      name: 'Prodotto',
-      price: 9.99,
-    },
-  )
+  const cookies = new Cookies()
+  const cartList = cookies.get('/cart')
   return (
     <div className="container p-2 mt-2">
       <div className="p-2 md-04dp rounded">
-        <CartItem name={arrayOfProducts[0].name} price={arrayOfProducts[0].price} />
+        {
+          cartList ? cartList.map(({ name, price }) => <CartItem name={name} price={price} />) : 'Vuoto'
+        }
+
       </div>
       <div className="p-2 d-flex justify-content-center">
         <button type="submit" className="w-auto rounded p-2 mt-4 fs-4 border-0 bg-site-primary">Noleggia subito</button>
