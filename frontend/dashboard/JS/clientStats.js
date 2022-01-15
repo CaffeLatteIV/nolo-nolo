19d449cff566a0aaef6fa327e3691a49ea1f2936
bosslibra <alessandrolibralesso@gmail.com>
2022-01-15 12:41:31 +0100

    dashboard should be fully accessible

--- frontend/dashboard/JS/clientStats.js
+++ frontend/dashboard/JS/clientStats.js
@@ -49,7 +49,25 @@ const genderConfig = {
 var genderChart = new Chart(document.getElementById("genderChart"), genderConfig);
 
 $(document).ready(function () {
-
-
-
+    //Inject gender data for accessibility
+    var maschi = genderData.datasets[0].data[0]
+    var femmine = genderData.datasets[0].data[1]
+    var nonSpec = genderData.datasets[0].data[2]
+    $('#maschi').html(maschi)
+    $('#maschi').html(maschi)
+    $('#maschi').html(maschi)
+    var fOne = genderData.datasets[0].data[0]
+    var fTwo = genderData.datasets[0].data[1]
+    var fThree = genderData.datasets[0].data[2]
+    var fFourth = genderData.datasets[0].data[3]
+    var fFifth = genderData.datasets[0].data[4]
+    var fSixth = genderData.datasets[0].data[5]
+    var fSeventh = genderData.datasets[0].data[6]
+    $('#f1').html(fOne)
+    $('#f2').html(fTwo)
+    $('#f3').html(fThree)
+    $('#f4').html(fFourth)
+    $('#f5').html(fFifth)
+    $('#f6').html(fSixth)
+    $('#f7').html(fSeventh)
 })
