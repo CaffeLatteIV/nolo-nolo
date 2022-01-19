e8fe6a8dd6d2cd277a2b28094b6acce4be4f6bdc
Mattia <mattiababbini99@gmail.com>
2022-01-19 15:04:21 +0100

    aggiunta logica per searchbar

--- frontend/frontoffice/src/components/Searchbar.js
+++ frontend/frontoffice/src/components/Searchbar.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Searchbar() {
+  const [searchText, setSearchText] = useState('')
+  const navigate = useNavigate()
+  function search(event) {
+    event.preventDefault()
+    navigate('/search', { state: { searchText } })
+  }
   return (
     <div>
       <div className="container-fluid w-100 md-04dp">
-        <form className="d-flex">
-          <input className="form-control me-1 bg-transparent text-white" type="search" placeholder="Cerca.." aria-label="Cerca" />
+        <form className="d-flex" onSubmit={search}>
+          <input className="form-control me-1 bg-transparent text-white" type="search" placeholder="Cerca.." aria-label="Cerca" value={searchText} onInput={(e) => setSearchText(e.target.value)} />
           <button className="btn text-black bg-site-primary" type="submit">Cerca</button>
         </form>
       </div>
