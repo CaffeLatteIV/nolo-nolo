99b5536576de68ab6cb0d03c348d4bd19eada185
bosslibra <alessandr.libralesso@studio.unibo.it>
2022-01-19 12:11:01 +0100

    add nav, footer and searchbar to backoffice

--- frontend/backoffice/src/main.js
+++ frontend/backoffice/src/main.js
@@ -1,5 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import 'material-icons/iconfont/material-icons.css';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/main.css';
 
 createApp(App).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
