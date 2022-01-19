d124582cf4e9f0739cca96618d3d883da17eb691
Mattia <mattiababbini99@gmail.com>
2022-01-19 15:04:51 +0100

    cambio link ad a per forzare caricamento pagina

--- frontend/frontoffice/src/components/Categories.js
+++ frontend/frontoffice/src/components/Categories.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const URL = process.env.INVENTORY_URL || 'http://localhost:5000/v1/inventories'
 
@@ -26,7 +26,7 @@ function Categories() {
           <ul className="navbar-nav">
             {categories ? categories.map((category) => (
               <li key={Math.random()} className="nav-item">
-                <Link className="nav-link active text-white" title={category} to={`/category?id=${category}`}>{category}</Link>
+                <a className="nav-link active text-white" title={category} href={`/category?id=${category}`}>{category}</a>
               </li>
             )) : ''}
           </ul>
