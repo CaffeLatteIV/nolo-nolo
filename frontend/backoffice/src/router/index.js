241e56b95132ecfaab82f73d9dd7d255957a4f57
bosslibra <alessandr.libralesso@studio.unibo.it>
2022-01-20 20:46:51 +0100

    add Inventory

--- frontend/backoffice/src/router/index.js
+++ frontend/backoffice/src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "" */ '../views/ClientInfo.vue')
+  },
+  {
+    path: '/admin/inventory',
+    name: 'Inventory',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "" */ '../views/Inventory.vue')
   }
 ]
 
