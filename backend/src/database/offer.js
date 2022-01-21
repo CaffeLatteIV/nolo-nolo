b48d2995e40c495a39f04243040dbb7fe5c72315
Mattia <mattiababbini99@gmail.com>
2022-01-21 16:57:24 +0100

    aggiunta gestione offerte

---
+++ backend/src/database/offer.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import { offerSchema } from './schema.js'
+
+class Offer {
+  constructor() {
+    this.URL = process.env.URL || 'mongodb://localhost:27017/nolo-nolo'
+    this.connect()
+  }
+
+  async connect() {
+    this.mongoose = await mongoose.connect(this.URL)
+    this.Offer = mongoose.model('offers', offerSchema)
+  }
+
+  async getOffer(start, end) {
+    // cerca un'offerta che abbia l'inizio o la fine nel range cercato
+    return this.Offer.find({
+      $or: [
+        { start: { $gte: start, $lte: end } },
+        { end: { $gte: start, $lte: end } }],
+    }).exec()
+  }
+}
+export default Offer
