606fb8575218182c3f00c6c9ebb2e536d34a441f
Mattia <mattiababbini99@gmail.com>
2022-01-23 11:49:06 +0100

    cambio gestione calcolo costi che deve tenre conto di offerte e fidelityPoints

--- frontend/frontoffice/src/pages/Receipt.js
+++ frontend/frontoffice/src/pages/Receipt.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable dot-notation */
-import React from 'react'
-import moment from 'moment-business-days'
+import React, { useEffect, useState } from 'react'
 // import PropTypes from 'prop-types'
 import { useNavigate, useLocation } from 'react-router-dom'
 import Cookie from 'universal-cookie'
@@ -8,21 +8,48 @@ import axios from 'axios'
 import validateAccessToken from '../components/Tokens.js'
 
 const URL = process.env.ORDERS_URL || 'http://localhost:5000/v1/rentals'
+const URL_OFFERS = process.env.OFFERS_URL || 'http://localhost:5000/v1/offers'
 function Receipt() {
   const navigate = useNavigate()
   const { state } = useLocation()
   const { newRent, start, end } = state
   if (!newRent) navigate('*')
   const daysBetweenDates = Math.ceil((end - start) / (1000 * 60 * 60 * 24))
-  const businessDays = moment(end).businessDiff(moment(start))
-  const holidayDays = daysBetweenDates - businessDays
-  let price = ((newRent.price.weekdays * businessDays) || 0) + ((newRent.price.weekends * holidayDays) || 0)
-  if (price <= 0) {
-    price = (new Date(start).getDay() % 6) ? newRent.price.weekday : newRent.price.weekend
-  }
   const earnedFidelityPoints = daysBetweenDates * newRent.fidelityPoints
+  // count weekends tra le due date
+  const [price, setPrice] = useState(0)
+  const [spentFidelityPoints, setSpentFidelityPoints] = useState(0)
+  useEffect(async () => {
+    console.log(start)
+    const { data } = await axios.get(`${URL_OFFERS}/`, { headers: { 'Content-Type': 'application/json' }, params: { start, end } })
+    const { offers } = data
+    let priceTmp = 0
+    let spentFidelityPointsTmp = 0
+    for (let i = start; i < end; i += 86400000) {
+      let priceDay = 0
+      const day = new Date(i)
+      const isWeekend = day.getDay() === 0 || day.getDay() === 6
+      if (newRent.spendablefidelityPoints > 0 && newRent.spendablefidelityPoints - newRent.price.fidelityPoints > 0) {
+        newRent.spendablefidelityPoints -= newRent.price.fidelityPoints
+        spentFidelityPointsTmp += newRent.price.fidelityPoints
+      } else if (isWeekend) {
+        priceDay = newRent.price.weekend
+      } else {
+        priceDay = newRent.price.weekday
+      }
+      offers.forEach((offer) => {
+        if (i >= offer.start && i <= offer.end) {
+          priceDay = (priceTmp * 100) / (100 - offer.discount)
+        }
+      })
+      priceTmp += priceDay
+    }
+    setSpentFidelityPoints(spentFidelityPointsTmp)
+    setPrice(priceTmp)
+  }, [])
   async function handleConfirm() {
-    validateAccessToken()
+    await validateAccessToken()
+
     const cookie = new Cookie()
     const client = cookie.get('client')
     const product = {
@@ -34,7 +61,7 @@ function Receipt() {
       clientCode: client.id,
       price,
       status: 'Prenotato', // se l'utente conferma lo status diventa prenotato
-      fidelityPoints: newRent.fidelityPoints || 0,
+      fidelityPoints: spentFidelityPoints,
     }
     console.log(product)
     console.log(client)
