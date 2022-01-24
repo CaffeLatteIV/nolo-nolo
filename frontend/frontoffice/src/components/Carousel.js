f2fecb056bb10ef58e7fdf57596d4e5be1d1803b
Mattia <mattiababbini99@gmail.com>
2022-01-24 16:37:37 +0100

    min bug fix

--- frontend/frontoffice/src/components/Carousel.js
+++ frontend/frontoffice/src/components/Carousel.js
@@ -7,7 +7,9 @@ function Carousel() {
   const [carouselItems, setCarouselItems] = useState(undefined)
   useEffect(async () => {
     const { data } = await axios.get(`${URL}/bestSellers?n=3`)
-    if (data.bestSellers.length === 0) {
+    const { bestSellers } = data
+    // eslint-disable-next-line no-extra-boolean-cast
+    if (!!bestSellers) {
       setCarouselItems(undefined)
     } else {
       setCarouselItems(data.bestSellers)
@@ -26,16 +28,16 @@ function Carousel() {
             <div className="carousel-item active" data-bs-interval="4000">
               <img src={carouselItems[0].media.img} className="d-block w-100" alt="First Item" style={{ maxHeight: '50vh' /* this will be obsolete with use of personalized images that fit the actual content */ }} />
               <div className="carousel-caption">
-                <h5>{ carouselItems[0].title }</h5>
-                <p>Prezzo: ${ carouselItems[0].price.weekday }/day</p>
+                <h5>{carouselItems[0].title}</h5>
+                <p>Prezzo: ${carouselItems[0].price.weekday}/day</p>
               </div>
             </div>
             {carouselItems[1] ? (
               <div className="carousel-item" data-bs-interval="3000">
                 <img src={carouselItems[1].media.img} className="d-block w-100" alt="Second Item" style={{ maxHeight: '50vh' }} />
                 <div className="carousel-caption">
-                  <h5>{ carouselItems[1].title }</h5>
-                  <p>Prezzo: ${ carouselItems[1].price.weekday }/day</p>
+                  <h5>{carouselItems[1].title}</h5>
+                  <p>Prezzo: ${carouselItems[1].price.weekday}/day</p>
 
                 </div>
               </div>
@@ -45,8 +47,8 @@ function Carousel() {
               <div className="carousel-item" data-bs-interval="3000">
                 <img src={carouselItems[2].media.img} className="d-block w-100" alt="Second Item" style={{ maxHeight: '50vh' }} />
                 <div className="carousel-caption">
-                  <h5>{ carouselItems[2].title }</h5>
-                  <p>Prezzo: ${ carouselItems[2].Prezzo.weekday }/day</p>
+                  <h5>{carouselItems[2].title}</h5>
+                  <p>Prezzo: ${carouselItems[2].Prezzo.weekday}/day</p>
 
                 </div>
               </div>
