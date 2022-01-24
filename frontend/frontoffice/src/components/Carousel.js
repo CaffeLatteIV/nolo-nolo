f19437e807219a175b78b5b5c5624db6640846f1
Mattia <mattiababbini99@gmail.com>
2022-01-24 16:51:02 +0100

    minor fix carousel

--- frontend/frontoffice/src/components/Carousel.js
+++ frontend/frontoffice/src/components/Carousel.js
@@ -8,8 +8,7 @@ function Carousel() {
   useEffect(async () => {
     const { data } = await axios.get(`${URL}/bestSellers?n=3`)
     const { bestSellers } = data
-    // eslint-disable-next-line no-extra-boolean-cast
-    if (!!bestSellers) {
+    if (!bestSellers) {
       setCarouselItems(undefined)
     } else {
       setCarouselItems(data.bestSellers)
@@ -20,39 +19,18 @@ function Carousel() {
       <div className="p-2 md-02dp rounded">
         <div id="carouselDark" className="carousel carousel-dark slide p-0 mb-0" data-bs-ride="carousel">
           <div className="carousel-indicators">
-            {carouselItems[0] ? <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" /> : ''}
-            {carouselItems[1] ? <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="1" aria-label="Slide 2" /> : ''}
-            {carouselItems[2] ? <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="2" aria-label="Slide 3" /> : ''}
+            {carouselItems.map((item) => (<button key={item.id} type="button" data-bs-target="#carouselDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" />))}
           </div>
           <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval="4000">
-              <img src={carouselItems[0].media.img} className="d-block w-100" alt="First Item" style={{ maxHeight: '50vh' /* this will be obsolete with use of personalized images that fit the actual content */ }} />
-              <div className="carousel-caption">
-                <h5>{carouselItems[0].title}</h5>
-                <p>Prezzo: ${carouselItems[0].price.weekday}/day</p>
-              </div>
-            </div>
-            {carouselItems[1] ? (
-              <div className="carousel-item" data-bs-interval="3000">
-                <img src={carouselItems[1].media.img} className="d-block w-100" alt="Second Item" style={{ maxHeight: '50vh' }} />
+            {carouselItems.map((item) => (
+              <div key={item.id} className="carousel-item active" data-bs-interval="3000">
+                <img src={item.media.img} className="d-block w-100" alt="Item" style={{ maxHeight: '50vh' /* this will be obsolete with use of personalized images that fit the actual content */ }} />
                 <div className="carousel-caption">
-                  <h5>{carouselItems[1].title}</h5>
-                  <p>Prezzo: ${carouselItems[1].price.weekday}/day</p>
-
+                  <h5>{item.title}</h5>
+                  <p>Prezzo: ${item.price.weekday}/day</p>
                 </div>
               </div>
-            ) : ''}
-
-            {carouselItems[2] ? (
-              <div className="carousel-item" data-bs-interval="3000">
-                <img src={carouselItems[2].media.img} className="d-block w-100" alt="Second Item" style={{ maxHeight: '50vh' }} />
-                <div className="carousel-caption">
-                  <h5>{carouselItems[2].title}</h5>
-                  <p>Prezzo: ${carouselItems[2].Prezzo.weekday}/day</p>
-
-                </div>
-              </div>
-            ) : ''}
+            ))}
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselDark" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true" />
