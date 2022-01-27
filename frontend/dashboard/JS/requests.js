7bd706bb8437e072938a6256038ea8089b7f081f
Mattia <mattiababbini99@gmail.com>
2022-01-27 17:54:58 +0100

    aggiunto getProduct e modificato bestSeller

--- frontend/dashboard/JS/requests.js
+++ frontend/dashboard/JS/requests.js
@@ -1,4 +1,5 @@
 const OPERATION_URL = 'http://localhost:5000/v1/operations'
+const INVENTORY_URL = 'http://localhost:5000/v1/inventories'
 const TOKEN_URL = 'http://localhost:5000/v1/token'
 
 async function bestSellers() {
@@ -6,7 +7,7 @@ async function bestSellers() {
   const cookie = new UniversalCookie()
   const accessToken = cookie.get('accessToken')
   return $.ajax({
-    url: `${OPERATION_URL}/bestSellers`,
+    url: `${OPERATION_URL}/bestSellers?n=3`,
     type: 'GET',
     dataType: "json",
     headers: { "Authorization": "Bearer " + accessToken },
@@ -45,6 +46,28 @@ async function getConditions(title = '') {
     headers: { "Authorization": "Bearer " + accessToken },
   })
 }
+async function groupClientAge() {
+  await validateAccessToken()
+  const cookie = new UniversalCookie()
+  const accessToken = cookie.get('accessToken')
+  return $.ajax({
+    url: `${OPERATION_URL}/clients/count/age/`,
+    type: 'GET',
+    dataType: "json",
+    headers: { "Authorization": "Bearer " + accessToken },
+  })
+}
+async function countClientGender() {
+  await validateAccessToken()
+  const cookie = new UniversalCookie()
+  const accessToken = cookie.get('accessToken')
+  return $.ajax({
+    url: `${OPERATION_URL}/clients/count/gender/`,
+    type: 'GET',
+    dataType: "json",
+    headers: { "Authorization": "Bearer " + accessToken },
+  })
+}
 async function avgRentMonth(title = '') {
   await validateAccessToken()
   const cookie = new UniversalCookie()
@@ -65,6 +88,13 @@ async function avgRentLength(title = '') {
     type: 'GET',
     dataType: "json",
     headers: { "Authorization": "Bearer " + accessToken },
+  })
+}
+async function getProduct(id) {
+  return $.ajax({
+    url: `${INVENTORY_URL}/products/${id}`,
+    type: 'GET',
+    dataType: "json",
   })
 }
 async function validateAccessToken() {
@@ -94,9 +124,6 @@ async function validateAccessToken() {
       }
 
     }
-
-
-
   })
 }
-export { getMonthlyRevenue, getStatus, getConditions, avgRentMonth, avgRentLength,bestSellers }
+export { getMonthlyRevenue, getStatus,getProduct, groupClientAge, countClientGender, getConditions, avgRentMonth, avgRentLength, bestSellers }
