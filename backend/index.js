1fe9ae31e79c1acdc6ebbe101f8bffb2dfbea24d
Mattia <mattiababbini99@gmail.com>
2022-02-03 11:06:26 +0100

    magia

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ import Express from 'express'
 import cors from 'cors'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import mongoose from 'mongoose'
+// import history from 'connect-history-api-fallback'
 // import loggerWrapper from './src/logger.js'
-import rental from './src/v1/rental-api.js'
+// import rental from './src/v1/rental-api.js'
 // import inventory from './src/v1/inventory-api.js'
 // import client from './src/v1/client-api.js'
 // import employee from './src/v1/employee-api.js'
@@ -23,10 +25,31 @@ const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+// app.use(
+//   history({
+//     rewrites: [
+//       {
+//         from: /management-dashboard(\W|\w)*/,
+//         to: '/management-dashboard',
+//       },
+//       {
+//         from: /^\/v1\/.*$/,
+//         to(context) {
+//           return context.parsedUrl.path
+//         },
+//       },
+//       {
+//         from: /\/(\W|\w)*/,
+//         to: '/',
+//       },
+//     ],
+//     disableDotRule: false,
+//   }),
+// )
 app.use(cors(corsOptions))
 app.use(Express.json())
 
-app.use('/v1/rentals', rental)
+// app.use('/v1/rentals', rental)
 // app.use('/v1/inventories', inventory)
 // app.use('/v1/clients', client)
 // app.use('/v1/employee', employee)
@@ -35,6 +58,14 @@ app.use('/v1/rentals', rental)
 // app.use('/v1/token', token)
 // app.use('/v1/offers', offers)
 // app.use(Express.static(path.join(__dirname, 'frontoffice')))
+
+const URL = 'mongodb://site202151:aixaem7T@mongo_site202151?writeConcern=majority'
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Connected to Mongo!')
+})
 app.get('/site/', (req, res) => res.sendFile(path.join(__dirname, 'frontoffice', 'index.html')))
 app.listen(PORT, () => {
   // logger.info(`Listening on port ${PORT}`)
