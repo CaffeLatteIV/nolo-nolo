d5a87f9bc6b974cbca488773c1eb9fd339a256b5
Mattia <mattiababbini99@gmail.com>
2022-02-08 11:20:15 +0100

    cambio livello di autenticazione per richieste

--- backend/src/v1/operation-api.js
+++ backend/src/v1/operation-api.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authenticateAccessToken, authenticateUserRole } from '../utils/authenticate.js'
+import { authenticateAccessToken, authenticateManager } from '../utils/authenticate.js'
 import Operation from '../database/operations.js'
 
 const app = express.Router()
@@ -19,56 +19,56 @@ app.get('/search/:title', async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.findSimilarTitle(title) })
 })
-app.get('/clients/count/age', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/clients/count/age', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.groupClientAge() })
 })
-app.get('/clients/count/gender', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/clients/count/gender', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.countGender() })
 })
-app.get('/rentals/revenue/month/:title', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/revenue/month/:title', authenticateAccessToken, authenticateManager, async (req, res) => {
   const { title } = req.params
   const operation = new Operation()
   return res.send({ result: await operation.getRevenueByMonth(title) })
 })
-app.get('/rentals/revenue/month/', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/revenue/month/', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.getRevenueByMonth() })
 })
-app.get('/rentals/count/status/:title', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/count/status/:title', authenticateAccessToken, authenticateManager, async (req, res) => {
   const { title } = req.params
   const operation = new Operation()
   return res.send({ result: await operation.countStatus(title) })
 })
-app.get('/rentals/count/status/', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/count/status/', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.countStatus() })
 })
-app.get('/rentals/count/conditions/:title', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/count/conditions/:title', authenticateAccessToken, authenticateManager, async (req, res) => {
   const { title } = req.params
   const operation = new Operation()
   return res.send({ result: await operation.countConditions(title) })
 })
-app.get('/rentals/count/conditions/', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/count/conditions/', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.countConditions() })
 })
-app.get('/rentals/single/avg/:title', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/single/avg/:title', authenticateAccessToken, authenticateManager, async (req, res) => {
   const { title } = req.params
   const operation = new Operation()
   return res.send({ result: await operation.avgRentLength(title) })
 })
-app.get('/rentals/single/avg/', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/single/avg/', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.avgRentLength() })
 })
-app.get('/rentals/month/avg/:title', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/month/avg/:title', authenticateAccessToken, authenticateManager, async (req, res) => {
   const { title } = req.params
   const operation = new Operation()
   return res.send({ result: await operation.avgRentByMonth(title) })
 })
-app.get('/rentals/month/avg/', authenticateAccessToken, authenticateUserRole, async (req, res) => {
+app.get('/rentals/month/avg/', authenticateAccessToken, authenticateManager, async (req, res) => {
   const operation = new Operation()
   return res.send({ result: await operation.avgRentByMonth() })
 })
