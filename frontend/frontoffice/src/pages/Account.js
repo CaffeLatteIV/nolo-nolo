827b914ef1f47e8d875e1444ba3074836672868b
Mattia <mattiababbini99@gmail.com>
2022-02-09 11:53:44 +0100

    rimosso messaggio minatorio di alessandro x2

--- frontend/frontoffice/src/pages/Account.js
+++ frontend/frontoffice/src/pages/Account.js
@@ -15,14 +15,6 @@ function Account() {
     <div className="container p-2">
       <div className="container md-01dp mt-4 rounded p-4">
         <h1 className="mb-4 text-center">Informazioni Account</h1>
-        <span className="text-danger">
-          MATTIA i valori che sono messi non sono placeholder, devi passarli tu
-          nell&apos;attributo value dei tag input (per il radio input è
-          l&apos;attributo checked) perchè così puoi sia vedere i valori che
-          modificarli. Fa così anche instagram direi. Quantomeno ho preso
-          ispirazione da loro. Ricordati di cancellare questo messaggio quando
-          hai finito.
-        </span>
         <div className="w-50 m-auto tab-container row">
           <div className="col mb-4 p-2 text-center">
             <button type="button" className={`w-100 h-100 text-white ${isPersonal ? 'fw-bold border-bottom' : ' border-bottom border-secondary'} border-3`} id="tabController1" onClick={goToPersonalInfo}>Dati Personali</button>
