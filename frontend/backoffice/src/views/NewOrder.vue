cea48080b4e289e943443070c63c8209dd0c72f5
bosslibra <alessandr.libralesso@studio.unibo.it>
2022-02-12 15:16:56 +0100

    add Datepicker

--- frontend/backoffice/src/views/NewOrder.vue
+++ frontend/backoffice/src/views/NewOrder.vue
@@ -1,35 +1,54 @@
 <template>
   <div class="container md-01dp mt-4 p-4 rounded">
-    <h1>Creazione Nuovo Ordine</h1>
-    <div id="newItemForm">
-      <div>
+    <h1 class="mb-4">Creazione Nuovo Ordine</h1>
+    <div id="newItemForm" class="w-50 m-auto">
+      <div class="row">
+        <div class="col">
+          <label for="inventorySelection"
+            >Prodotto:
+            <select
+              name="inventory"
+              id="inventorySelection"
+              class="form-select"
+              v-model="selectedProduct"
+            >
+              <option disabled value="">Seleziona il prodotto</option>
+              <option
+                v-for="item in inventory"
+                :key="item"
+                :value="(product = { code: item.id, title: item.title })"
+              >
+                {{ item.title }}
+              </option>
+            </select>
+          </label>
+        </div>
+      </div>
+      <div class="col">
         <label for="inventorySelection"
-          >Prodotto:
-          <select
-            name="inventory"
-            id="inventorySelection"
-            class="form-select"
-            v-model="selectedProduct"
-          >
-            <option disabled value="">Seleziona il prodotto</option>
-            <option v-for="item in inventory" :key="item" :value="item.id">
-              {{ item.title }}
-            </option>
-          </select>
+          >Seleziona data:
+          <Datepicker v-model="date" range></Datepicker>
         </label>
       </div>
     </div>
+    {{ selectedProduct }}
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Cookies from "universal-cookie";
+import Datepicker from "vue3-date-time-picker";
+import "vue3-date-time-picker/dist/main.css";
 
 export default {
   name: "NewOrder",
+  components: {
+    Datepicker,
+  },
   data() {
     return {
+      date: null,
       loading: true,
       inventory: [],
       clientCode: this.$route.params.id,
@@ -55,6 +74,7 @@ export default {
           console.log(this.inventory);
         });
     },
+    handleConfirm() {},
   },
 };
 </script>
