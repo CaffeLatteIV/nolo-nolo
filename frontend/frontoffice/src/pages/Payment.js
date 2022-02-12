7d3172b5fee1700d5d566feff36f0137ea891568
Mattia <mattiababbini99@gmail.com>
2022-02-12 12:18:57 +0100

    gestione pagamento + sommario costi

--- frontend/frontoffice/src/pages/Payment.js
+++ frontend/frontoffice/src/pages/Payment.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Cookies from 'universal-cookie'
+import validateAccessToken from '../components/Tokens.js'
 
+const RENTALS_URL = process.env.RENTALS_URL || 'http://localhost:5000/v1/rentals'
 function Payment() {
-  const { newRent } = useState([])
-  const { daysBetweenDates } = useState([])
-  const { spentFidelityPoints } = useState([])
-  const { earnedFidelityPoints } = useState([])
-  const { coupon } = useState([])
-  const { price } = useState([])
-  const lateFee = 80378
-  //   handleConfirm(){
-  //     console.log("modify me")
-  //   }
-  return (
+  const { state } = useLocation()
+  const { productCode, id } = state
+  const [lateFee, setLateFee] = useState('')
+  const [price, setPrice] = useState('')
+  const [rentData, setRentData] = useState(undefined)
+  const [daysBetween, setdaysBetween] = useState('')
+  useEffect(async () => {
+    await validateAccessToken()
+    const cookie = new Cookies()
+    const accessToken = cookie.get('accessToken')
+    const { data } = await axios.post(`${RENTALS_URL}/paymentInfo/${id}`, {}, { headers: { Authorization: `Bearer ${accessToken}` } })
+
+    setPrice(data.fee > 0 ? data.rent.price + data.fee : data.rent.price)
+    setRentData(data.rent)
+    setdaysBetween(data.daysBetween)
+    setLateFee(data.fee)
+  }, [])
+  const navigate = useNavigate()
+  async function handlePay() {
+    await validateAccessToken()
+    const cookie = new Cookies()
+    const accessToken = cookie.get('accessToken')
+    await axios.post(`${RENTALS_URL}/pay/${id}`, {}, { headers: { Authorization: `Bearer ${accessToken}` } })
+    navigate('/orders')
+  }
+  return rentData ? (
     <div className="container p-2">
       <div className="md-01dp p-4 mt-4 rounded">
         <h1>Riepilogo Ordine</h1>
@@ -19,59 +39,59 @@ function Payment() {
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Codice prodotto:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{newRent}</div>
+          <div className="col-sm-6 col-md-9">{productCode}</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Titolo:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{newRent}</div>
+          <div className="col-sm-6 col-md-9">{rentData.productCode.title}</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Descrizione:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{newRent}</div>
+          <div className="col-sm-6 col-md-9">{rentData.productCode.description}</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Durata noleggio:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{daysBetweenDates} giorn{daysBetweenDates === 1 ? 'o' : 'i'}</div>
+          <div className="col-sm-6 col-md-9">{Math.floor((rentData.end - rentData.start) / 86400000)} giorni{daysBetween > 0 ? ` + ${daysBetween} di ritardo` : ' '}</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Prezzo giorni feriali:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{newRent}€</div>
+          <div className="col-sm-6 col-md-9">{rentData.productCode.price.weekday}€</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
-            <span className="fw-bold">Prezzo giorni festivi:</span>
+            <span className="fw-bold">Prezzo weekend:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{newRent}</div>
+          <div className="col-sm-6 col-md-9">{rentData.productCode.price.weekend}€</div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Punti fedeltà spesi:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{spentFidelityPoints}
+          <div className="col-sm-6 col-md-9">{rentData.fidelityPoints}
           </div>
         </div>
         <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Punti fedeltà guadagnati:</span>
           </div>
-          <div className="col-sm-6 col-md-9">{earnedFidelityPoints}
+          <div className="col-sm-6 col-md-9">{rentData.earnedFidelityPoints}
           </div>
         </div>
-        <div className="row border-bottom border-secondary p-2">
+        {/* <div className="row border-bottom border-secondary p-2">
           <div className="col-sm-6 col-md-3">
             <span className="fw-bold">Sconto applicato:</span>
           </div>
           <div className="col-sm-6 col-md-9">{coupon ? `${coupon}%` : '-'}
           </div>
-        </div>
+        </div> */}
         { (lateFee > 0) ? (
           <div className="row border-bottom border-secondary p-2">
             <div className="col-sm-6 col-md-3">
@@ -91,12 +111,13 @@ function Payment() {
         <button
           type="button"
           className="btn text-black mt-4 bg-site-primary"
+          onClick={handlePay}
         >
           Paga e restituisci
         </button>
       </div>
     </div>
-  )
+  ) : 'caricamento...'
 }
 
 export default Payment
