cebbd279964e3f170c351fd033e09ed5086a1b58
bosslibra <alessandr.libralesso@studio.unibo.it>
2022-02-14 00:23:13 +0100

    should be ok

--- frontend/frontoffice/src/components/ActiveOrders.js
+++ frontend/frontoffice/src/components/ActiveOrders.js
@@ -8,20 +8,29 @@ function ActiveOrders({ productCode, id, title, price, start, end, img, handleDe
     <>
       <div className="p-2 px-3">
         <div className="row">
-          <Link to={`/product?id=${productCode}`} className="product-card-link">
-            <div className="col-2 p-2">
+          <div className="col-lg-2 col-sm-12 p-2">
+            <Link to={`/product?id=${productCode}`} className="product-card-link">
               <img src={img} className="card-img" alt="Item Pic" />
+            </Link>
+          </div>
+          <div className="col-lg-7 col-sm-12 p-2">
+            <h4 className="text-wrap text-white">{ title }</h4>
+            <p className=" ps-4text-white">{price}€</p>
+          </div>
+          <div className="col-lg-3 col-sm-12 p-2 m-0 text-white text-center">
+            <div className="row">
+              <div className="col-lg-6 col-sm-9">
+                Da: { dayjs(start).format('DD/MM/YYYY') }&nbsp;
+                A: { dayjs(end).format('DD/MM/YYYY') }
+              </div>
+              <div className="col-lg-6 col-sm-3">
+                <button type="button" className="bg-transparent rounded border-0" onClick={() => handleDelete(id)}>
+                  <span className="material-icons text-white pt-2">delete</span>
+                </button>
+              </div>
             </div>
-          </Link>
-          <div className="col-7 p-2">
-            <h4 className="m-0 text-wrap text-white">{ title }</h4>
-            <p className="text-white">{price}€</p>
           </div>
-          <div className="col-lg-3 p-2 m-0 text-white text-center">
-            Da: { dayjs(start).format('DD/MM/YYYY') }&nbsp;
-            A: { dayjs(end).format('DD/MM/YYYY') }
-          </div>
-          <button type="button" onClick={() => handleDelete(id)}>delete</button>
+
         </div>
       </div>
       <hr />
