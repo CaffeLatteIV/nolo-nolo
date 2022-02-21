023b95dfda4bc5d28e1cfdc882f529e0ddef4661
Mattia <mattiababbini99@gmail.com>
2022-02-21 19:58:41 +0100

    typo

--- backend/src/v1/image-api.js
+++ backend/src/v1/image-api.js
@@ -6,12 +6,10 @@ const app = express.Router()
 app.get('/:filename', (req, res) => {
   const { filename } = req.params
   const fullfilepath = path.join(global.rootDir, `/images/${filename}`)
-  if(fs.existsSync(fullfilepath)){
+  if (fs.existsSync(fullfilepath)) {
     return res.sendFile(fullfilepath)
-
-  }else{
-    return res.sendFile(path.join(global.rootDir, `/src/images/${filename}`))
   }
+  return res.sendFile(path.join(global.rootDir, `/src/images/${filename}`))
 })
 
 export default app
